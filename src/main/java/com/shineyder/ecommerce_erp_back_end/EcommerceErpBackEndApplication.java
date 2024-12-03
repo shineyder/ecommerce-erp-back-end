365f421ad7ad0b9d785622456b8de853b623cffd
Adriano Shineyder <adrianoshineyder@hotmail.com>
2024-12-03 11:49:27 -0300

    Initial commit

---
+++ src/main/java/com/shineyder/ecommerce_erp_back_end/EcommerceErpBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.shineyder.ecommerce_erp_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceErpBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceErpBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shineyder/ecommerce_erp_back_end/EcommerceErpBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shineyder.ecommerce_erp_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceErpBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
